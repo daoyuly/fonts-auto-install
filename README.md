1be29c7fd646fbf6d72672662403ea282df3ee48
daoyu@umu.com <daoyu@umu.com>
2022-10-02 13:47:11 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # fonts-auto-install
+
+## How to install?
+
+### Mac
+```shell
+curl -L https://github.com/daoyuly/fonts-auto-install/raw/master/install-mac.sh | bash
+```
+
+### Linux
+```shell
+curl -L https://github.com/hbin/top-programming-fonts/raw/master/install-linux.sh | bash
+```
+
+
+## Contribute
+Inspired by [top-programming-fonts](https://github.com/hbin/top-programming-fonts) and [fonts](https://github.com/powerline/fonts)
---
+++ install-linux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+FONTS_DIR=$HOME/Library/Fonts
+FONTS_LIST=("Menlo-Regular.ttf" "DejaVuSansMono.ttf" "FiraCode-Regular.ttf" "Inconsolata-Regular.ttf")
+
+function die () {
+    echo "${@}"
+    exit 1
+}
+
+cat <<EOF
+--------------------------
+    Fonts Auto Install
+--------------------------
+EOF
+
+if [[ ! -e $FONTS_DIR ]]; then
+    mkdir $FONTS_DIR || die "Could not make $FONTS_DIR"
+fi
+
+for i in ${FONTS_LIST[*]}; do
+    echo "Downloading $i"; echo
+    wget -c https://github.com/daoyuly/fonts-auto-install/raw/master/fonts/$i \
+      || die "Fail to download ${i}"
+    mv $i $FONTS_DIR || die "Could not install $i"
+    echo "Installed $i successfully"; echo
+done
+
+cat <<EOF
+--------------------------
+    Fonts installed!
+--------------------------
+EOF
